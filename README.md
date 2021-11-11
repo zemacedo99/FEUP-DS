334157c08997ab22078559a4e27a2ff75ea97efe
Mariana Truta <marianatruta2000@gmail.com>
2021-11-11 22:22:09 +0000

    setup is done; needs some tests

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ds-meic56
+# Scrum Book (ds-meic56)
+
+## Docker
+
+### Install
+Firstly, you'll need to have Docker and Docker Compose installed on your computer.
+- [Install Docker](https://docs.docker.com/get-docker/)
+- [Install Docker Compose](https://docs.docker.com/compose/install/#install-compose)
+
+**Note:** Docker Desktop for Windows/Mac includes Compose, so you don't need to install it.
+
+### Build and run our app with Compose
+1. From your project directory, start up your application by running the following command:
+   ```
+   docker-compose up --build
+   ```
+2. Navigate on your browser to:
+   - http://localhost:3000/ to see the application running;
+   - http://localhost:3001/ to access to backend.
+
+### Adding a dependency to project
+A volume is used to store the node modules so when we add a dependency to package.json, we need to delete these volumes and restart the application.
+We can do that with the following command:
+   ```
+   docker-compose down -v && docker-compose up --build
+   ```    
+**Note:** if there is a better way to keep the docker modules updated, please update this section.
+
+## Useful links
+- [React](https://reactjs.org/docs/getting-started.html): this page is an overview of the React documentation and related resources.
+- [React Bootstrap](https://react-bootstrap.github.io/getting-started/introduction/): similar to bootstrap.
+- [React-Icons](https://react-icons.github.io/react-icons/): include popular icons in your React projects easily with react-icons, which utilizes ES6 imports that allows you to include only the icons that your project is using.
+- [Package.json guide](https://nodejs.dev/learn/the-package-json-guide): if you want to understand how package.json works.
+
+## Example
+At this moment, the project has a basic example how to use routes, react-bootstrap and how the communication between backend and frontend works without an api.
---
+++ backend/app.js
@@ -0,0 +1,67 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const logger = require('morgan');
+
+const port = 3000;
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening on port: ${port}`);
+});
+
+module.exports = app;

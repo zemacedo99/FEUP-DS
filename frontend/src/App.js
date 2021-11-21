19438e224d9b8f9def68d9aa22ea066aff3180c1
fuscati <andre@andre-laptop.localdomain>
2021-11-21 12:38:24 +0000

    working db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/style.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Example from './pages/Example';
-import firebase from './utils/firebase'
+import db from './utils/firebase'
+
+import { collection, getDocs } from "firebase/firestore"; 
 
 function App() {
+
+const querySnapshot = getDocs(collection(db, "patlets")).then((obj)=>{
+
+            const docSnapshots = obj.docs;
+            
+            for (let i in docSnapshots) {
+                console.log( docSnapshots[i].data().title)
+            } 
+        })
+
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" exact element={<Example/>}/>
-                </Routes>
-            </div>
-        </Router>
+        
+        <div>
+            <h1>hello</h1>
+        </div>
+        // <Router>
+        //     <div className="App">
+        //         <Routes>
+        //             <Route path="/" exact element={<Example/>}/>
+        //         </Routes>
+        //     </div>
+        // </Router>
     );
 }
 
--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Cloud Firestore through Firebase
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyCJb7N9JOW9Ru11jgvrBwUH7ohDZrQIW20",
   authDomain: "ds-meic05.firebaseapp.com",
   projectId: "ds-meic05",
@@ -14,8 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "917825385962",
   appId: "1:917825385962:web:bce0f2f9fafee68b5dee93",
   measurementId: "G-NJS86QX3K7"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(firebaseApp);
+
+export default db;

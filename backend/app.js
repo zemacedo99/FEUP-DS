f7644bb3c1a0a5b4bdad25b345ca177acf2bedb7
Mariana Truta <marianatruta2000@gmail.com>
2021-11-24 10:51:14 +0000

    added eslint and removed unnecessary files

--- backend/app.js
+++ backend/app.js
@@ -12,18 +12,12 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use((req, res, next) => {

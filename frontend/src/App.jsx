374dfc029b5d5b2ce46b2384b4593750912f3b96
André Nascimento <andre2000.an@gmail.com>
2021-12-04 13:00:14 +0000

    Help and About Us Page initial design (lorem ipsum)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Example from './pages/Example';
+import HelpAboutUs from './pages/HelpAboutUs';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" exact element={<Example />} />
+          <Route path="/help" exact element={<HelpAboutUs />} />
         </Routes>
       </div>
     </Router>

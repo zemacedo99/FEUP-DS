a640a3a3118d3ff9540967c026a01b9449b8b73c
André Nascimento <andre2000.an@gmail.com>
2021-12-15 22:57:12 +0000

    Passing text as children property to SubTitleText component

--- frontend/src/pages/About/index.jsx
+++ frontend/src/pages/About/index.jsx
@@ -18,8 +18,24 @@ export default function About() {
             Varius eget suscipit cras magnis elit non molestie mattis.
             Risus dictum enim, egestas mauris.
           </Text>
-          <SubTitleText title="Scrum" text="Diam diam est neque quam sapien proin. Amet pulvinar ipsum, aliquet nunc euismod. Dui faucibus fusce auctor non adipiscing eget. Sed feugiat suspendisse in amet massa, neque. Lorem nunc euismod nulla pellentesque fusce orci commodo et rutrum. Mattis nunc, in vestibulum mi faucibus amet, lectus. Ultrices lacus commodo netus odio pellentesque consequat pellentesque. Ut est malesuada nam lacus, aenean." />
-          <SubTitleText title="Pattern" text="Quis est ultricies sagittis nec. Risus massa dictumst commodo, ac. Eget id interdum habitasse neque vel pellentesque pharetra, malesuada tortor. Faucibus quam donec vitae condimentum tristique hac fermentum feugiat. Lacus, viverra diam elit integer fermentum penatibus quam. Neque proin varius ultrices interdum malesuada mauris diam, consectetur." />
+          <SubTitleText title="Scrum">
+            Diam diam est neque quam sapien proin.
+            Amet pulvinar ipsum, aliquet nunc euismod.
+            Dui faucibus fusce auctor non adipiscing eget.
+            Sed feugiat suspendisse in amet massa, neque.
+            Lorem nunc euismod nulla pellentesque fusce orci commodo et rutrum.
+            Mattis nunc, in vestibulum mi faucibus amet, lectus.
+            Ultrices lacus commodo netus odio pellentesque consequat pellentesque.
+            Ut est malesuada nam lacus, aenean.
+          </SubTitleText>
+          <SubTitleText title="Pattern">
+            Quis est ultricies sagittis nec.
+            Risus massa dictumst commodo, ac.
+            Eget id interdum habitasse neque vel pellentesque pharetra, malesuada tortor.
+            Faucibus quam donec vitae condimentum tristique hac fermentum feugiat.
+            Lacus, viverra diam elit integer fermentum penatibus quam.
+            Neque proin varius ultrices interdum malesuada mauris diam, consectetur.
+          </SubTitleText>
         </Col>
         <Col xs={12} md={6} className="ps-md-5">
           <Title className="mb-4 mt-sm-0 mt-3">Logo</Title>
@@ -33,7 +49,14 @@ export default function About() {
             Ultrices lacus commodo netus odio pellentesque consequat pellentesque.
             Ut est malesuada nam lacus, aenean.
           </Text>
-          <SubTitleText title="About Us" text="Quis est ultricies sagittis nec. Risus massa dictumst commodo, ac. Eget id interdum habitasse neque vel pellentesque pharetra, malesuada tortor. Faucibus quam donec vitae condimentum tristique hac fermentum feugiat. Lacus, viverra diam elit integer fermentum penatibus quam. Neque proin varius ultrices interdum malesuada mauris diam, consectetur." />
+          <SubTitleText title="About Us">
+            Quis est ultricies sagittis nec.
+            Risus massa dictumst commodo, ac.
+            Eget id interdum habitasse neque vel pellentesque pharetra, malesuada tortor.
+            Faucibus quam donec vitae condimentum tristique hac fermentum feugiat.
+            Lacus, viverra diam elit integer fermentum penatibus quam.
+            Neque proin varius ultrices interdum malesuada mauris diam, consectetur.
+          </SubTitleText>
         </Col>
       </Row>
     </Layout>

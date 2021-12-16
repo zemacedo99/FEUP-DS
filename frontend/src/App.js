01aa134fe00617bdd37f30d73600937250207dce
fuscati <andre@andre-laptop.localdomain>
2021-12-16 12:16:57 +0000

    solve duplicate bug; add a more intuitive example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/style.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Example from './pages/Example';
-import db from './utils/firebase'
+import DatabaseExample from './pages/DatabaseExample';
 
-import { collection, getDocs } from "firebase/firestore"; 
 
 function App() {
 
-    //exemplo de ir buscar dados à db (está a retornar duas vezes por motivos desconhecidos)
-    const querySnapshot = getDocs(collection(db, "patlets")).then((obj)=>{
 
-            const docSnapshots = obj.docs;
-            
-            for (let i in docSnapshots) {
-                console.log( docSnapshots[i].data().title)
-            } 
-        })
 
     return (
 
          <Router>
              <div className="App">
                  <Routes>
-                     <Route path="/" exact element={<Example/>}/>
+                     <Route path="/" exact element={<DatabaseExample/>}/>
                  </Routes>
              </div>
          </Router>

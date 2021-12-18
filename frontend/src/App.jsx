3fc7cb0775e76f49b289c3932333a78097ea9fb7
João Pinto <joaocastropintoz@gmail.com>
2021-12-18 16:26:29 +0000

    need to commit to merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// import Example from './pages/Example';
-import DatabaseExample from './pages/DatabaseExample';
+import { NavbarCustomDesktop, NavbarCustomTablet, NavbarCustomMobile } from './components/Navbar';
+import HelpAboutUs from './pages/HelpAboutUs';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavbarCustomDesktop />
+        <NavbarCustomTablet />
+        <NavbarCustomMobile />
         <Routes>
-          <Route path="/" exact element={<DatabaseExample />} />
+          <Route path="/" exact element={<MainPage />} />
+          <Route path="/search" exact element={<SearchPage />} />
+          <Route path="/help" exact element={<HelpAboutUs />} />
         </Routes>
       </div>
     </Router>

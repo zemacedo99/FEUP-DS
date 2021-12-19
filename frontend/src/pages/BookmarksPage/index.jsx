ec6d81fb0478dc10c805b2b7ba26c2d9d974cc28
Ricardo Marques <ricardomarquescarvalho2000@outlook.pt>
2021-12-19 21:24:52 +0000

    finished readLater + favorites page, read from local storage, refreshing dynamically when clicking on buttons favorite + readLater

--- frontend/src/pages/BookmarksPage/index.jsx
+++ frontend/src/pages/BookmarksPage/index.jsx
@@ -5,10 +5,18 @@ import BookmarksSelector from '../../components/BookmarksSelector';
 import PatternCardList from '../../components/PatternCardList';
 import patterns from '../../placeholders/PlaceholderPatterns';
 import { Layout, PageTitle } from '../../style';
+import AddBookmark from '../AddBookmark';
+import AddFavorite from '../AddFavorite';
 
 export default function BookmarksPage() {
   const [patternsList, setPatterns] = useState(patterns);
   const [isFavoriteList, setFavoriteList] = useState(true);
+  const [favoriteIds, setFavoriteIds] = useState(JSON.parse(localStorage.getItem('favorites')));
+  const [bookmarkIds, setBookmarkIds] = useState(JSON.parse(localStorage.getItem('bookmarks')));
+
+  if (!localStorage.getItem('patterns')) {
+    localStorage.setItem('patterns', JSON.stringify(patterns));
+  }
 
   const updatePattern = (pattern) => {
     const index = patternsList.findIndex((item) => item.id === pattern.id);
@@ -17,8 +25,15 @@ export default function BookmarksPage() {
     setPatterns([...list]);
   };
 
-  const favoriteList = patternsList.filter((e) => e.favorite);
-  const readlaterList = patternsList.filter((e) => e.saved);
+  const storagePatterns = JSON.parse(localStorage.getItem('patterns'));
+  const favoriteList = favoriteIds ? storagePatterns.filter(
+    (e) => favoriteIds[e.id],
+  ) : [];
+
+  const readlaterIds = JSON.parse(localStorage.getItem('bookmarks'));
+  const readlaterList = bookmarkIds ? storagePatterns.filter(
+    (e) => readlaterIds[e.id],
+  ) : [];
 
   return (
     <Layout>
@@ -35,6 +50,12 @@ export default function BookmarksPage() {
       <PatternCardList
         patterns={isFavoriteList ? favoriteList : readlaterList}
         updatePattern={updatePattern}
+      />
+      <AddFavorite
+        setFavoriteIds={setFavoriteIds}
+      />
+      <AddBookmark
+        setBookmarkIds={setBookmarkIds}
       />
     </Layout>
   );

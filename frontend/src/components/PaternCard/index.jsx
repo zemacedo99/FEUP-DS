57f50e1af5f29f052715a7a70227cf00d6e9373d
BrenoAccioly <breno.accioly91@gmail.com>
2021-12-19 21:03:53 +0000

    Integrated fav/bookmarks in main page

--- frontend/src/components/PaternCard/index.jsx
+++ frontend/src/components/PaternCard/index.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
 import { Card, Dropdown } from 'react-bootstrap';
-import {
-  AiFillClockCircle,
-  AiFillStar,
-  AiOutlineClockCircle,
-  AiOutlineStar,
-} from 'react-icons/all';
 
 import PropTypes from 'prop-types';
 
+import { Bookmark } from '../Bookmark/index';
+import { Favorite } from '../Favorite/index';
 import {
   PatCard, CardTitle, Icon, Toggle, DropdownSection, DropdownItem,
 } from './style';
 
 export default function PatternCard({
-  id, name, image, favorite, saved, updatePattern,
+  id, name, image,
 }) {
   const goToPattern = () => {}; // TODO
-
-  const buildPattern = (newFav, newSaved) => ({
-    id,
-    name,
-    favorite: newFav,
-    saved: newSaved,
-    image,
-  });
-
-  const favoriteButton = (event) => {
-    event.stopPropagation();
-    const pattern = buildPattern(!favorite, saved);
-    updatePattern(pattern);
-  };
-
-  const saveButton = (event) => {
-    event.stopPropagation();
-    updatePattern(buildPattern(favorite, !saved));
-  };
 
   return (
     <PatCard title="See pattern">
@@ -45,30 +22,12 @@ export default function PatternCard({
           <Icon />
         </Toggle>
         <Dropdown.Menu>
-          {favorite ? (
-            <DropdownItem onClick={(event) => favoriteButton(event)} className="d-flex align-items-center">
-              <AiFillStar className="me-2" />
-              <span>Unfavorite</span>
-            </DropdownItem>
-          )
-            : (
-              <DropdownItem onClick={(event) => favoriteButton(event)} className="d-flex align-items-center">
-                <AiOutlineStar className="me-2" />
-                <span>Favorite</span>
-              </DropdownItem>
-            )}
-          {saved ? (
-            <DropdownItem onClick={(event) => saveButton(event)} className="d-flex align-items-center">
-              <AiFillClockCircle className="me-2" />
-              Unsave
-            </DropdownItem>
-          )
-            : (
-              <DropdownItem onClick={(event) => saveButton(event)} className="d-flex align-items-center">
-                <AiOutlineClockCircle className="me-2" />
-                Save for later
-              </DropdownItem>
-            )}
+          <DropdownItem className="d-flex align-items-center">
+            <Favorite patlet_id={id} />
+          </DropdownItem>
+          <DropdownItem className="d-flex align-items-center">
+            <Bookmark patlet_id={id} />
+          </DropdownItem>
         </Dropdown.Menu>
       </DropdownSection>
       <Card.Body onClick={goToPattern}>
@@ -82,7 +41,4 @@ PatternCard.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  favorite: PropTypes.bool.isRequired,
-  saved: PropTypes.bool.isRequired,
-  updatePattern: PropTypes.func.isRequired,
 };

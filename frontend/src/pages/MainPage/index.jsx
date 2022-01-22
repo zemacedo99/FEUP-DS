e443a3229255d18e9206fdca428192f6fa3b3b06
BrenoAccioly <breno.accioly91@gmail.com>
2022-01-22 19:42:17 +0000

    Removed icon

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
-import { useNavigate } from 'react-router-dom';
 
 import axios from 'axios';
 
-import infoIcon from '../../assets/infoIcon.svg';
 import PatternCardList from '../../components/PatternCardList';
 import { Layout, PageTitle } from '../../style';
 import { SubTitle } from './style';
 
 export default function MainPage() {
-  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [patternsList, setPatterns] = useState([]);
   const setFavoriteIds = useState(JSON.parse(localStorage.getItem('favorites')))[1];
@@ -35,20 +32,11 @@ export default function MainPage() {
     setPatterns([...list]);
   };
 
-  const navigateToHelp = () => {
-    navigate('/about');
-  };
-
   return (
     <Layout>
       <Row className="align-items-center">
         <Col>
           <PageTitle> Home </PageTitle>
-        </Col>
-        <Col className="d-flex justify-content-end pe-4">
-          <span onClick={() => navigateToHelp()} onKeyPress={() => navigateToHelp()} role="button" tabIndex={0}>
-            <img src={infoIcon} width="30px" alt="info icon" title="Help" style={{ cursor: 'pointer' }} />
-          </span>
         </Col>
       </Row>
       <SubTitle> The core of Scrum </SubTitle>
